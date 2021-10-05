0a310e011be4d007c5071ab7b7dd57cc1be4ebba
Ben Swenson <info@benswenson.net>
2021-10-05 23:00:56 +0100

    added new database details

--- connection.php
+++ connection.php
@@ -1,5 +1,9 @@
 <?php
-$link = mysqli_connect("localhost", "benswens_CarShare", "andrew77", "benswens_CarShare");
+//Development Connection
+//$link = mysqli_connect("localhost", "benswens_CarShare", "andrew77", "benswens_CarShare");
+
+//Remote Database Connection
+$link = mysqli_connect("remotemysql.com", "uQff1yb2Cd", "mgOSYzNmFz", "uQff1yb2Cd");
 if(mysqli_connect_error()){
 	die("ERROR: Unable to connect: " . mysqli_connect_error());
 }
